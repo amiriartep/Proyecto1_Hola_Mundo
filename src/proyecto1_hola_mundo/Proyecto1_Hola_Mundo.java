c158242c24359dea95eaf5e9776212b08a4e2255
Usuario7 <Usuario7@FI4531119-07.unal.loc>
2016-02-09 03:20:23 -0500

    new commit gh

--- src/proyecto1_hola_mundo/Proyecto1_Hola_Mundo.java
+++ src/proyecto1_hola_mundo/Proyecto1_Hola_Mundo.java
@@ -4,7 +4,7 @@ package proyecto1_hola_mundo;
 public class Proyecto1_Hola_Mundo {
 
     public static void main(String[] args) {
-       System.out.println("Hola ");
+       System.out.println("Hola mundo");
     }
     
 }
